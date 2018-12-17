6324292cb4aa23ee0c0bf9f3407aa749c05ff449
lambor.cai <lamborcai@gmail.com>
2018-12-17 15:49:28 +0800

    新文件：    demo/client/main.go 	新文件：    demo/server/main.go

---
+++ demo/server/main.go
@@ -0,0 +1,41 @@
+// go:generate protoc -I ../helloworld --go_out=plugins=grpc:../helloworld ../helloworld/helloworld.proto
+
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	pb "github.com/chat-rpc/go-chat/demo/helloworld"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port	= ":50051"
+)
+
+//Server type
+type server struct{}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply,error) {
+	return &pb.HelloReply{Message:"Hello " + in.Name} , nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp",port)
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v" ,err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s,&server{})
+
+	reflection.Register(s)
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to save: %v", err)
+	}
+}
